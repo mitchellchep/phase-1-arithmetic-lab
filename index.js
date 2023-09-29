6f1480f65cb33df96d697717a0e0dc1e8bbd6ab1
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-09-29 15:08:50 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+// Create a variable called num1 and set it to a value
+const num1 = 31;
 
-// Write your code here
+// Create a variable called num2 and set it to a value
+const num2 = 2;
+
+// Create a variable called multiply to multiply num1 and num2, resulting in 62
+const multiply = num1 * num2;
+
+
+const num3 = 64;
+
+
+const num4 = 10;
+
+
+const mod = num3 % num4;
+
+
+const numberArray = [5, 10, 15, 20];
+
+
+const max = Math.max(...numberArray);
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log("multiply:", multiply);
+console.log("random:", random);
+console.log("mod:", mod);
+console.log("max:", max);
